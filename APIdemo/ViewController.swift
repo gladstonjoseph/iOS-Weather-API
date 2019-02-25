2bd1d8fb6f20300aead67cba4a6f7419bb4f2bc0
Gladston Joseph <gladstonjoseph@gladstons-macbook-air.local>
2019-02-25 19:21:58 +0530

    Initial Commit

---
+++ APIdemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APIdemo
+//
+//  Created by Gladston Joseph on 2/25/19.
+//  Copyright © 2019 Gladston Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ APIdemoTests/APIdemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  APIdemoTests.swift
+//  APIdemoTests
+//
+//  Created by Gladston Joseph on 2/25/19.
+//  Copyright © 2019 Gladston Joseph. All rights reserved.
+//
+
+import XCTest
+@testable import APIdemo
+
+class APIdemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
